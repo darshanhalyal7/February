f795ce69aed633cbb30e735af19071d12ef8afc6
darshanhalyal7 <darshanhalyal@gmail.com>
2026-02-09 19:38:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const yesBtn = document.getElementById("yesBtn");
+const noBtn = document.getElementById("noBtn");
+const response = document.getElementById("response");
+
+yesBtn.addEventListener("click", () => {
+  response.innerHTML = "YAY!! 💕 I can’t wait to celebrate with you 😘";
+});
+
+noBtn.addEventListener("mouseover", () => {
+  const x = Math.random() * 200 - 100;
+  const y = Math.random() * 200 - 100;
+  noBtn.style.transform = `translate(${x}px, ${y}px)`;
+});
